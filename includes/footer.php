7356ba2ff5d82b4b6f372c261bfe153412d16f25
tonycondone <touyboateng339@gmail.com>
2025-08-06 15:40:07 +0000

    auto: Wed, Aug  6, 2025  3:40:07 PM

--- includes/footer.php
+++ includes/footer.php
@@ -53,7 +53,8 @@
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">DataShare Platform</strong> <span>All Rights Reserved</span></p>
+      <p>Powered by TTU</p>
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">DataShare Platform</strong> <span>2025</span> <span>All Rights Reserved</span></p>
       <div class="credits">
         DataShare Platform - Dataset Sharing and Collaboration Platform
       </div>
