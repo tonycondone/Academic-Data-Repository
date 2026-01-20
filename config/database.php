cb28e858f0d992f3bf696c2bbcc46a10b55d6a47
tonycondone <touyboateng339@gmail.com>
2026-01-20 19:34:25 +0000

    Db host connection Updated for Vercel

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ class Database {
 
     public function __construct() {
         // Database connection (Updated for Vercel)
-        $this->host     = getenv('DB_HOST') ?: 'localhost';
+        $this->host     = getenv('DB_HOST') ?: '127.0.0.1';
         $this->db_name  = getenv('DB_NAME') ?: 'dataset_platform';
         $this->username = getenv('DB_USER') ?: 'root';
         $this->password = getenv('DB_PASS') ?: '1212';
